8c4fae03e58ca0da76cd8c8af1ebd39104217257
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-07-25 19:39:07 +0200

    add sign-in & user pages components

--- argentbank/src/pages/Sign.js
+++ argentbank/src/pages/Sign.js
@@ -2,8 +2,26 @@ import React from "react";
 
 export default function sign() {
   return (
-    <div>
-      <h1>Sign-in</h1>
-    </div>
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form>
+          <div className="input-wrapper">
+            <label for="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="input-wrapper">
+            <label for="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="input-remember">
+            <label for="remember-me">Remember me</label>
+            <input type="checkbox" id="remember-me" />
+          </div>
+          <button className="sign-in-button">Sign In</button>
+        </form>
+      </section>
+    </main>
   );
 }
--- argentbank/src/pages/User.js
+++ argentbank/src/pages/User.js
@@ -1,9 +1,22 @@
 import React from "react";
+import UserHeader from "../components/User/UserHeader";
+import Account from "../components/User/Account";
+import { accountDatas } from "../data/account-datas";
 
 export default function user() {
   return (
-    <div>
-      <h1>User</h1>
-    </div>
+    <main className="main bg-dark">
+      <UserHeader />
+      <h2 className="sr-only">Accounts</h2>
+      {accountDatas.map((obj, index) => {
+        return (
+          <Account
+            title={obj.title}
+            amount={obj.amount}
+            description={obj.description}
+          />
+        );
+      })}
+    </main>
   );
 }
