8c23a0668e92953ba2cfb84ca246b5e577a2ff80
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-08-14 21:34:24 +0200

    display error in authentication

--- argentbank/src/pages/Login.js
+++ argentbank/src/pages/Login.js
@@ -28,6 +28,9 @@ export class Login extends React.Component {
       sessionStorage.setItem("token", answer.body.token);
       return this.props.loginAction(answer.body.token);
     }
+    else if (answer.data.status === 401) {
+      alert("Email ou mot de passe incorrect")
+    }
   }
 
   render() {
